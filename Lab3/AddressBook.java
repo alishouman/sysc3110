7063d214824ccde1e0cea588aa4e2af9e71f9d23
alishouman <alishouman@4377-011.ODENG2.CARLETON.CA>
2014-09-25 15:41:19 -0400

    ds

---
+++ Lab3/AddressBook.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.*;
+
+
+public class AddressBook {
+
+
+		private List<BuddyInfo> buddyInfo;
+		public AddressBook(){
+		this.buddyInfo=new ArrayList<BuddyInfo>();}
+			
+		
+public void addBuddy(BuddyInfo aBuddyInfo){
+	if(aBuddyInfo!=null){
+		this.buddyInfo.add(aBuddyInfo);
+		
+	}
+	
+}
+public BuddyInfo removeBuddy(int index){
+	if(index>=0&& index <this.buddyInfo.size()){
+		return this.buddyInfo.remove(index);
+	}
+	return null;
+}
+	
+public static void main(String[] args) {
+	BuddyInfo buddy=new BuddyInfo("Tom", "Carleton",1234);
+	AddressBook addressBook=new AddressBook();
+	addressBook.addBuddy(buddy);
+	addressBook.removeBuddy(0);
+	System.out.print("hi");
+}
+}

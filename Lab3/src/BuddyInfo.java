08d13b8881261b704742af739d4c023788260ea2
alishouman <alishouman@4377-011.ODENG2.CARLETON.CA>
2014-09-25 15:38:08 -0400

    oooo

---
+++ Lab3/src/BuddyInfo.java
@@ -0,0 +1,11 @@
+
+public class BuddyInfo {
+private String name;
+private String address;
+private int telephone;
+public BuddyInfo(String name,String address,int telephone){
+	this.name=name;
+	this.address=address;
+	this.telephone=telephone;
+}
+}
